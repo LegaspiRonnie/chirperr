d3f8b9b4d300d25a690ba03346cf030777c1fce3
Ronnie Legaspi <ronnielegaspi98@gmail.com>
2026-07-02 22:06:16 +0800

    initial commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="lofi">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ isset($title) ? $title . ' - Chirperr' : 'Chirperr'}}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <!-- Tailwind CSS -->
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+        <!-- DaisyUI -->
+        <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+
+    </head>
+    <body >
+        <!-- NAV BAR HERE -->
+
+        <!-- HERO -->
+         <main class="flex-1 container p-5 bg-base-300 text-base-content text-xs">
+            {{ $slot }}
+         </main>
+    </body>
+</html>
